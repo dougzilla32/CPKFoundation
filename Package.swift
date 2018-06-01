237cb4b9a165055f337125e3dadcdab266afa3c1
dougzilla <dougzilla32@gmail.com>
2018-05-31 22:43:49 -0700

    Update the Swift Package Manager files to use 'CancelForPromiseKit' (rather than the old 'CancellablePromiseKit')

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "CPKFoundation",
     dependencies: [
-        .Package(url: "https://github.com/dougzilla32/CancellablePromiseKit", majorVersion: 1)
+        .Package(url: "https://github.com/dougzilla32/CancelForPromiseKit", majorVersion: 1)
     ],
     exclude: [
 		"Tests"  // currently SwiftPM is not savvy to having a single test…
