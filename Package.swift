5ec383647fa35db89af9747e2e95449dea98ff8f
dougzilla <dougzilla32@gmail.com>
2018-08-05 23:10:42 -0700

    Update SPM config files to include appropriate minor version number for dependencies
    Update SPM config files to use '.git' suffix when specifying dependencies, to match the PromiseKit convention

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "CPKFoundation",
     dependencies: [
-        .Package(url: "https://github.com/PromiseKit/Foundation", majorVersion: 3),
-        .Package(url: "https://github.com/dougzilla32/CancelForPromiseKit", majorVersion: 1)
+        .Package(url: "https://github.com/PromiseKit/Foundation.git", majorVersion: 3, minor: 1),
+        .Package(url: "https://github.com/dougzilla32/CancelForPromiseKit.git", majorVersion: 1, minor: 1)
     ],
     exclude: [
 		"Tests"  // currently SwiftPM is not savvy to having a single test…
